b9ad6f77232cb3031575c652d68066c905765380
Ataar <mateenataar07@gmail.com>
2025-11-11 22:54:30 +0530

    first-commit

--- src/app/shared/components/comp-c/comp-c.component.ts
+++ src/app/shared/components/comp-c/comp-c.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./comp-c.component.scss']
 })
 export class CompCComponent implements OnInit {
-   @Input() message!:string
+   @Input() grandchild!:string
   constructor() { }
 
   ngOnInit(): void {
